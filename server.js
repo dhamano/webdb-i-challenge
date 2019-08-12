0db01b22ed9167fe79b31b51d8dccacacf8c1efd
Dustin Hamano <dhamano@Dustins-iMac-Pro.local>
2019-08-12 13:29:47 -0700

    MVP and stretch done

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
+const accountsRouter = require('./routes/accounts');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter);
+
+server.get('/', (req, res) => res.send(`<h2>API Running</h2>`));
 
 module.exports = server;
